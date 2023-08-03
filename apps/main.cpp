3e4846a7ec833d62a367eb048e362a0d1a44581e
Pavlo Logvyn <vulcaanpasha@gmail.com>
2023-08-03 16:14:44 +0300

    Remove debug logging

--- apps/main.cpp
+++ apps/main.cpp
@@ -4,9 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "[main] Start program\n";
     elemvm::ElementaryVM vm;
     auto result = vm.run(argc, argv);
-    std::cout << "[main] End program\n";
     return result;
 };
